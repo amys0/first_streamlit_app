023ef8f2354fa0174a5b4dd38509d49a3cce6851
amys0 <123104589+amys0@users.noreply.github.com>
2023-01-25 14:22:23 -0500

    Update streamlit_app.py
    get all rows not just one

--- streamlit_app.py
+++ streamlit_app.py
@@ -41,7 +41,7 @@ import snowflake.connector
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 my_cur.execute("select * from pc_rivery_db.public.fruit_load_list")
-my_data_row = my_cur.fetchone()
+my_data_rows = my_cur.fetchall()
 streamlit.header("The fruit load list contains:")
-streamlit.dataframe(my_data_row)
+streamlit.dataframe(my_data_rows)
 
